e23a71f2aaa1e08c82b2df8dafe2f3a841b5cdc5
Tanner Townsend <rsmcwow@gmail.com>
2020-04-06 02:19:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount=20)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity=1)
+    self.total += price * quantity
+  end
+  
+  def apply_discount
+    self.total = self.total - (self.total * (self.discount/100))
+    binding.pry
+    self.total
+  end
+  
+end
